434a2c89837c0c75128f9b4366dab6b12d81be09
unknown <mirzamiftahulfadillah@gmail.com>
2022-03-26 16:04:48 +0700

    created RestaurantFull in resto_detail

--- lib/resto_detail.dart
+++ lib/resto_detail.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:yess_nutrion/api/api_service.dart';
 import 'package:yess_nutrion/model/resto.dart';
 import 'package:yess_nutrion/recomend_resto.dart';
@@ -7,7 +10,7 @@ import 'package:yess_nutrion/widget/drink_list.dart';
 import 'package:yess_nutrion/widget/food_list.dart';
 
 class DetailResto extends StatefulWidget {
-  static const routeName = '/detail_restaurant_page';
+  static const routeName = 'detail_restaurant_page';
   final Restaurant resto;
 
   const DetailResto({required this.resto});
@@ -17,116 +20,138 @@ class DetailResto extends StatefulWidget {
 }
 
 class _DetailRestoState extends State<DetailResto> {
-  @override
-  late Future<RestoList> _resto;
-
-  @override
-  void initState() {
-    super.initState();
-    _resto = ApiService().restaurantDetail(widget.resto.id);
-  }
 
   Widget build(BuildContext context) {
-    final RestaurantFull restofull;
+String id = 'rqdv5juczeskfw1e867';
     return Scaffold(
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage(widget.resto.pictureId),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(40),
-                            bottomRight: Radius.circular(40))),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              IconButton(
-                                icon: Icon(
-                                  Icons.arrow_back,
-                                  color: Colors.white,
-                                ),
-                                onPressed: () {
-                                  Navigator.pushReplacementNamed(
-                                      context, RecomendResto.routeName);
-                                },
-                              ),
-                            ]),
-                        SizedBox(
-                          height: 200,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.resto.name,
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                        Row(
+      
+      body: FutureBuilder(
+        future: get(Uri.parse('https://restaurant-api.dicoding.dev/detail/$id')),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          } else if (snapshot.hasData) {
+            final response = snapshot.data as Response;
+            final body = response.body;
+            final restaurantFull = RestaurantFull.fromJson(jsonDecode(body)['restaurant']);
+
+            return DetailRestoBody(restaurantFull: restaurantFull);
+          } else {
+            return CircularProgressIndicator();
+          }
+      },
+      ),
+    );
+  }
+}
+
+class DetailRestoBody extends StatelessWidget {
+  final RestaurantFull restaurantFull;
+
+  const DetailRestoBody({
+    required this.restaurantFull,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SingleChildScrollView(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage('https://restaurant-api.dicoding.dev/images/medium/${restaurantFull.pictureId}'),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            Icon(
-                              Icons.location_on,
-                              size: 16,
-                              color: secondaryColor,
+                            IconButton(
+                              icon: Icon(
+                                Icons.arrow_back,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                Navigator.pushReplacementNamed(
+                                    context, RecomendResto.routeName);
+                              },
                             ),
-                            SizedBox(
-                              width: 3,
-                            ),
-                            Text(widget.resto.city)
-                          ],
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Description',
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(widget.resto.description),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Text(
-                          'Foods',
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                        FoodList(resto: widget.resto),
-                        SizedBox(height: 30),
-                        Text(
-                          'Drinks',
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                        DrinkList(resto: resto),
-                      ],
-                    ),
+                          ]),
+                      SizedBox(
+                        height: 200,
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        restaurantFull.name,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            size: 16,
+                            color: secondaryColor,
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          Text(restaurantFull.city)
+                        ],
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        'Description',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(restaurantFull.description),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Foods',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      FoodList(resto: restaurantFull),
+                      SizedBox(height: 30),
+                      Text(
+                        'Drinks',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      DrinkList(resto: restaurantFull),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
